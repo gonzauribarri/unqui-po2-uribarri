6e268c9faf2507d00a0ef166b2bff0165c43a3d0
Gonzalo Uribarri <gonzalouribarri@hotmail.com>
2024-05-01 15:29:23 -0300

    Cambiando importacion a jupiter.Test por error de compilacion

--- test/ar/edu/unq/po2/tpsolid/BancoTest.java
+++ test/ar/edu/unq/po2/tpsolid/BancoTest.java
@@ -1,9 +1,8 @@
 package ar.edu.unq.po2.tpsolid;
 
 import static org.junit.Assert.assertEquals;
-
-import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class BancoTest {
 	private Banco banco;
@@ -15,16 +14,16 @@ public class BancoTest {
 	
 	@BeforeEach
 	public void setUp() {
-		banco = new Banco("BBVA");
-		tito = new Cliente("Tito", "Fontevoa", "Brandsen 2010", 20, 25000);
-		cacho = new Cliente("Cacho", "Fasiuto", "Rivadavia 3000", 30, 45000);
-		casaReferencia = new Propiedad("Casa de 4 ambientes en la localidad de Quilmes", "Cuelli 3835", 80000);
-		solicitudPersonal = new Solicitud(tito, 3000, 20);
-		solicitudHipotecaria = new SolicitudHipotecaria(cacho, 2500, 30, casaReferencia);
+		this.banco = new Banco("BBVA");
+		this.tito = new Cliente("Tito", "Fontevoa", "Brandsen 2010", 20, 25000);
+		this.cacho = new Cliente("Cacho", "Fasiuto", "Rivadavia 3000", 30, 45000);
+		this.casaReferencia = new Propiedad("Casa de 4 ambientes en la localidad de Quilmes", "Cuelli 3835", 80000);
+		this.solicitudPersonal = new SolicitudPersonal(tito, 3000, 20);
+		this.solicitudHipotecaria = new SolicitudHipotecaria(cacho, 2500, 30, casaReferencia);
 	}
 	
 	@Test
 	public void testCreandoBanco() {
-		assertEquals("BBVA", banco.getNombre());
+		assertEquals("BBVA", this.banco.getNombre());
 	}
 }
